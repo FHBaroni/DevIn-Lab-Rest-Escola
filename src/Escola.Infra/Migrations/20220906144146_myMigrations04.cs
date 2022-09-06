c22ec6c7240c136fb55a26c2ea7328b7c490a0fa
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-09-06 11:50:29 -0300

    criaado banco de dados

---
+++ src/Escola.Infra/Migrations/20220906144146_myMigrations04.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Escola.Infra.Migrations
+{
+    public partial class myMigrations04 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
